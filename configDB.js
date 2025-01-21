68af4e30d7a0effe1e74ee2ec41426ca5a88e6ac
Brian Elmo <brianelmo14@gmail.com>
2025-01-21 19:30:15 -0300

    agregando variables de entorno

--- configDB.js
+++ configDB.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
+dotenv.config();
 
 const connection = await mysql.createConnection({
-    host:'localhost',
-    user: 'brianelmo',
-    database: 'mercaditogamer',
-    password: 'briancale12',
+    host:process.env.HOST_DB,
+    user: process.env.USER_DB,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD_DB,
 })
 
 export default connection;
